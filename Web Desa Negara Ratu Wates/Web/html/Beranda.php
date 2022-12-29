59bbaedd61fec95b5c72f11280afada81b8102c3
anggrndewi <dewi.120140149@student.itera.ac.id>
2022-12-29 15:05:17 +0700

    change vendor

--- Web Desa Negara Ratu Wates/Web/html/Beranda.php
+++ Web Desa Negara Ratu Wates/Web/html/Beranda.php
@@ -14,9 +14,9 @@
 
   <link rel="stylesheet" href="../assets/css/bootstrap.css">
 
-  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+  <link rel="stylesheet" href="../assets/vendor/owl.carousel.css">
 
-  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+  <link rel="stylesheet" href="../assets/vendor/animate.css">
 
   <link rel="stylesheet" href="../assets/css/theme.css">
 </head>
